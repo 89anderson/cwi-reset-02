ba5764de95eb1d2c181fe0a195394c4d28d8bbb3
Anderson Martins <andeeer@gmail.com>
2021-02-23 06:43:44 -0300

    madrugando

--- banco-da-desgraca/src/br/com/banco/desgraca/BancoDaDesgraca.java
+++ banco-da-desgraca/src/br/com/banco/desgraca/BancoDaDesgraca.java
@@ -1,8 +1,6 @@
 package br.com.banco.desgraca;
 
 import br.com.banco.desgraca.domain.InstituicaoBancaria;
-import br.com.banco.desgraca.domain.TipoTransacao;
-import br.com.banco.desgraca.domain.Transacao;
 import br.com.banco.desgraca.domain.conta.ContaCorrente;
 
 import java.text.DecimalFormat;
@@ -15,12 +13,10 @@ public class BancoDaDesgraca {
         ContaCorrente corrente2 = new ContaCorrente(222, InstituicaoBancaria.CAIXA,1000.00);
 
         corrente1.depositar(100.00);
+        corrente1.sacar(50.00);
+        corrente1.transferir(100.00,corrente2);
 
-        /*System.out.printf("O saldo da conta %s é %s",
-                corrente1.toString(),DecimalFormat.getCurrencyInstance().format(corrente1.consultarSaldo()));
-        System.out.printf("\nO saldo da conta %s é %s",
-                corrente2.toString(),DecimalFormat.getCurrencyInstance().format(corrente2.consultarSaldo()));*/
-
+        //corrente1.exibirExtrato(01/07/2020,16/07/2020);
 
     }
 }
--- banco-da-desgraca/src/br/com/banco/desgraca/domain/conta/TiposDeConta.java
+++ banco-da-desgraca/src/br/com/banco/desgraca/domain/conta/TiposDeConta.java
@@ -4,6 +4,7 @@ import br.com.banco.desgraca.Data;
 import br.com.banco.desgraca.domain.InstituicaoBancaria;
 import br.com.banco.desgraca.domain.TipoTransacao;
 import br.com.banco.desgraca.domain.Transacao;
+import br.com.banco.desgraca.exception.SaldoInsuficienteException;
 
 import java.text.DecimalFormat;
 import java.time.LocalDate;
@@ -44,26 +45,50 @@ public abstract class TiposDeConta implements ContaBancaria {
 
     @Override
     public void depositar(Double valor) {
-        this.saldo = this.saldo + valor;
+        this.saldo += valor;
         transacoes.add(new Transacao(Data.getDataTransacao(), TipoTransacao.depositar,valor));
-        System.out.println(transacoes);
-        System.out.printf("Depositando %s na %s", DecimalFormat.getCurrencyInstance().format(valor),this.contaApresentacao());
+        //System.out.printf("Depositando %s na %s", DecimalFormat.getCurrencyInstance().format(valor),this.contaApresentacao());
 
 
     }
 
     @Override
     public void sacar(Double valor) {
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+            transacoes.add(new Transacao(Data.getDataTransacao(), TipoTransacao.sacar,valor));
+            System.out.printf("Sacando %s na %s", DecimalFormat.getCurrencyInstance().format(valor),this.contaApresentacao());
+        } else{
+            throw new RuntimeException("\n Ih... ta pobrinho pai?\nO seu saldo é insuficiente para realizar essa operação!");
+        }
 
     }
 
     @Override
     public void transferir(Double valor, ContaBancaria contaDestino) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            transacoes.add(new Transacao(Data.getDataTransacao(), TipoTransacao.transferir, valor));
+            contaDestino.depositar(valor);
+            System.out.printf("Sacando %s na %s", DecimalFormat.getCurrencyInstance().format(valor),this.contaApresentacao());
+        } else{
+            throw new RuntimeException("\n Moiô padrinho...\n" +
+                    "O seu saldo é insuficiente para realizar essa operação!");
+        }
 
     }
 
     @Override
     public void exibirExtrato(LocalDate inicio, LocalDate fim) {
 
+        for (int i = 0; i <= transacoes.size(); i++) {
+            System.out.printf("%s\n", transacoes.getClass());
+            //Thread.sleep(250);
+
+            /*int index = 0;
+            for (Imovel imovel : opcoesViaveis) {
+                System.out.printf(" [%d] %s\n", ++index, imovel.apresentacao());
+                Thread.sleep(250);*/
+        }
     }
 }
